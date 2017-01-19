d6a7b0573d9c3348018439e88ac25aafb5a9505c
Galushkin Pavel <gpg@ua.fm>
2017-01-19 16:32:51 +0200

    HW05_optional

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -1,14 +1,27 @@
 package ru.javawebinar.topjava;
 
-/**
- * User: gkislin
- * Date: 05.08.2015
- *
- * @link http://caloriesmng.herokuapp.com/
- * @link https://github.com/JavaOPs/topjava
- */
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.format("Hello Topjava Enterprise!");
+
+        // java 7 Automatic resource management
+        GenericXmlApplicationContext springContext = new GenericXmlApplicationContext();
+        springContext.getEnvironment().setActiveProfiles(Profiles.HSQLDB, Profiles.DATAJPA);
+        springContext.load("spring/spring-app.xml", "spring/spring-db.xml");
+        springContext.refresh();
+        System.out.println("Bean definition names: " + Arrays.toString(springContext.getBeanDefinitionNames()));
+            /*AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+            adminUserController.create(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
+            System.out.println();
+
+            MealRestController mealController = appCtx.getBean(MealRestController.class);
+            List<MealWithExceed> filteredMealsWithExceeded =
+                    mealController.getBetween(
+                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
+                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
+            filteredMealsWithExceeded.forEach(System.out::println);*/
     }
 }
